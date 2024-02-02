7f38cce1f8f35e8f320c6a5acc1b8f0c5ba40a42
labri70 <labri70@naver.com>
2024-02-02 14:07:11 +0900

    cache.yaml 재수정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd1
+          Learn GithubAction 2
         </a>
       </header>
     </div>
